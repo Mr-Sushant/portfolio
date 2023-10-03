73289f9af6eaa16ae919e520a85ae798de2ab663
Sushant Shukla <1805352@kiit.ac.in>
2023-10-03 13:36:29 +0530

    updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Sushant</span></h1>
-          <p className= {`${styles.heroSubText} mt-2 text-white-100`}>I develop Frontend <br className='sm:block hidden'/>and Backend applications</p>
+          <p className= {`${styles.heroSubText} mt-2 text-white-100`}>I develop Fullstack <br className='sm:block hidden'/>and Mobile applications</p>
         </div>
       </div>
         <ComputersCanvas />
